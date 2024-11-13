e1301156a6cb6ee87fe7d6c35c95d80c40a8115d
Ilya Tyamin <tyaminilya@gmail.com>
2024-11-14 00:19:49 +0300

    [fix] fix bug

--- restart-with-down.sh
+++ restart-with-down.sh
@@ -1,4 +1,6 @@
-docker-compose down --rmi all -v --remove-orphans
+docker stop yacontest_testbot-bot
+docker rm yacontest_testbot-bot
+docker image rm yacontest_testbot-bot
 git pull
 sync ; echo 1 > /proc/sys/vm/drop_caches
 docker-compose up
